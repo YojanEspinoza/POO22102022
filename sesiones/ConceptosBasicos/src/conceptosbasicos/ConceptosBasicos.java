c83997a4c1de32e885e492cf6bf390e061231179
Yojan_Espinoza <yojanespinoza18@aragon.unam.mx>
2022-03-04 13:48:17 -0600

    Codigo lenovo 4 de marzo

--- sesiones/ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
+++ sesiones/ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
@@ -3,11 +3,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package conceptosbasicos;
-
+import ico.fes.lenovo.Procesador;
 /**
  *
  * @author yojan
  */
+
 public class ConceptosBasicos {
 
     /**
@@ -16,6 +17,11 @@ public class ConceptosBasicos {
     public static void main(String[] args) {
      // TODO code application logic here
         System.out.println("hola otra vez");
-    }
+        
+        Procesador cpu = new Procesador();
+        cpu.setFrecuencia(5.3f);
+        System.out.println( cpu );
+        
+}
     
 }
